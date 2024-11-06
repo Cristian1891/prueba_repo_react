eef71b4927853bf6d07f33fdae53d5793d811eec
Cristian1891 <cristianovejero1891@gmail.com>
2024-11-06 19:11:41 -0300

    prueba1

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Components/Navbar'
+import ItemListContainer from './Components/ItemListContainer'
+
+
+
+function App() {
+  
+  return (
+    <>
+      < Navbar/>
+      < ItemListContainer greeting = "Bienvenidas a Poch. ropa para mujeres" />                
+    </>
+  )
+}
+
+export default App
